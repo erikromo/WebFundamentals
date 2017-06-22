b29c560b66770b97f5ebced56b0100beec03016b
Erik Romo <erik.romo@outlook.com>
2017-06-22 10:56:51 -0400

    New Stuff

---
+++ JavaScript_Basics/0.06_functions.js
@@ -0,0 +1,74 @@
+function hello(){
+	console.log("hello");
+}
+
+hello();
+
+
+var hey = function(){
+	console.log("Hey!")
+}
+
+hey();
+
+var m = function(){
+	console.log("Erik Romo");
+}
+
+m();
+
+function multiplication(){
+	var x= 3
+	var y=1100
+	console.log(x*y);
+}
+
+
+
+
+multiplication();
+
+
+function numberEntered(x){
+	console.log("The number entered is",x);
+}
+
+numberEntered (5);
+numberEntered(7);
+numberEntered(100000);
+
+function addAnyTwoNumbers(x,y){
+	console.log("The sum is",x+y);
+}
+
+addAnyTwoNumbers(17,1000);
+addAnyTwoNumbers(15,29);
+ 
+
+
+function 
+name(first,last){
+	console.log(first+","+last);
+}
+
+name("Erik","Romo")
+
+
+function petNameAndBreed(name,breed){
+	var details=name+","+breed;
+	console.log(details);
+}
+
+petNameAndBreed("Rufus","Weiner Dog");
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ JavaScript_Basics/0.06_functions_tax_calculator.js
@@ -0,0 +1,5 @@
+function taxcalculator(quantity,price){
+	var tax=0.07
+	var totalTax= quantity*price*tax;
+	var totalPrice=totalTax+quantity
+}
