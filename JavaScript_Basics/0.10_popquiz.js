1c43213feff40f2d74c8cd692ad22adcf406c538
Erik Romo <erik.romo@outlook.com>
2017-06-27 16:16:37 -0400

    worked on new things

---
+++ JavaScript_Basics/0.10_popquiz.js
@@ -0,0 +1,43 @@
+	var counter=0;
+		while(counter<100){
+			counter+=10;
+			if(counter===60){
+				console.log(" sixty ");	
+		}else{
+			console.log(counter);
+		}
+							}
+
+
+var cars = ["Saab", "Volvo", "BMW"];
+console.log (cars);
+
+
+
+
+function total(length,width){
+	
+	var total= length*width;
+	console.log(total);
+	
+}
+
+total(5,9)
+
+
+
+for ( var i=1;i< 51 ; i++){
+	if (i % 2 === 0 && i % 3 === 0 ){
+		console.log("MizzMuzz");
+	} else if (i % 3 === 0){
+		console.log("Muzz");
+	} else if (i % 2 ===0){
+		console.log("Mizz");{
+			else{
+				console.log(i);
+			}
+		}
+	}
+}
+
+
